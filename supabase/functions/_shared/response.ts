645484f324b24999e9748f7e0e95ff1983f89ccc
Riska Kurnia Dewi <riska@farmacare.id>
2026-09-01 20:58:12 +0800

    fix

--- supabase/functions/_shared/response.ts
+++ supabase/functions/_shared/response.ts
@@ -1,8 +1,9 @@
 export const corsHeaders = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Headers":
-        "authorization, x-client-info, apikey, content-type",
+        "authorization, x-client-info, apikey, apiKey, content-type, accept, origin, x-requested-with",
     "Access-Control-Allow-Methods": "GET, POST, PUT, PATCH, DELETE, OPTIONS",
+    "Access-Control-Max-Age": "86400",
 };
 
 export function response(
